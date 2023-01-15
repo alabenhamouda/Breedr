ec2cfede6342ddba8865c0e3f8363527c59afbff
GdragonT <taoufik_c10@hotmail.com>
2023-01-15 10:27:09 +0100

    remove useless

--- backend/src/util/entities/animalFilters.entity.ts
+++
@@ -1,8 +0,0 @@
-import { AnimalType } from "../enums/animal.enum";
-import { Gender } from "../enums/gender.enum";
-
-export class animalFilters{
-    animal: AnimalType
-    gender: Gender
-    breed: string
-}
